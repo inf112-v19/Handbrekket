0e020db249ea1edde2b3b09cc722a0b4539fa0a3
JonasLindaas <jonas.drageset.lindaas@gmail.com>
2019-01-30 12:38:23 +0100

    Expanded and improved comments

--- src/main/java/inf112/skeleton/app/ISquare.java
+++ src/main/java/inf112/skeleton/app/ISquare.java
@@ -1,17 +1,17 @@
 package inf112.skeleton.app;
 
-
-
 public interface ISquare {
     /**
+     * Returns the Robot currently in the Square
      *
      * @return Robot or NULL if empty
      */
     IRobot getRobot();
 
     /**
+     * Checks if there is a wall in the given direction
      *
-     * @param dir
+     * @param dir The direction to check
      * @return True if there is a wall, false otherwise.
      *
      */
@@ -19,10 +19,9 @@ public interface ISquare {
     boolean hasWall(int dir);
 
     /**
+     * Checks if the Square has any walls
      *
      * @return True if there are any walls, false otherwise.
      */
     boolean hasWalls();
-
-
 }

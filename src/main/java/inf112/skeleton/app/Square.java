a46afcb0173dc0c562b442879c5ba395237a26b8
Alba Gullerud <aledaable@Albas-MacBook-Pro.local>
2019-02-04 13:47:59 +0100

    laget sqaure klassen som implemterer ISquare og testklassen for Sqaure

---
+++ src/main/java/inf112/skeleton/app/Square.java
@@ -0,0 +1,32 @@
+package inf112.skeleton.app;
+
+
+import java.awt.*;
+
+public class Square implements ISquare{
+
+    public Square () {
+
+    }
+
+
+    public IRobot getRobot() {
+        if (Square)
+    }
+
+    public boolean hasWalls(){
+
+        return false;
+    }
+
+    public boolean hasWall(int dir){
+
+        return false;
+    }
+
+    private boolean isEmpty() {
+        if(Square){
+
+        }
+    }
+}
---
+++ src/test/java/inf112/skeleton/app/testSquare.java
@@ -0,0 +1,21 @@
+package inf112.skeleton.app;
+
+import org.junit.Test;
+
+public class testSquare {
+
+    @Test
+    public void gotRobot () {
+
+    }
+
+    @Test
+    public static void hasWalls(){
+
+    }
+
+    @Test
+    public static void hasWallsWithDirection() {
+        
+    }
+}

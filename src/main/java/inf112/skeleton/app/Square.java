6ffaa83bd92409f4bc05c66880f7cd34377a7d64
Alba Gullerud <aledaable@Albas-MacBook-Pro.local>
2019-02-06 08:16:42 +0100

    Gruppetime

--- src/main/java/inf112/skeleton/app/Square.java
+++ src/main/java/inf112/skeleton/app/Square.java
@@ -1,32 +1,44 @@
 package inf112.skeleton.app;
 
 
+
 import java.awt.*;
 
 public class Square implements ISquare{
 
-    public Square () {
+    private int length;
+    private int width;
+    private String state; //State -> empty or robot
+    private boolean wall;
 
+    public Square (int length, int width, String state, boolean wall) {
+        this.length=length;
+        this.width=width;
+        this.state=state;
+        this.wall=wall;
     }
 
 
     public IRobot getRobot() {
-        if (Square)
+        if(this.state=="robot"){
+            //return robotObjekt
+        } else if (this.state=="empty"){
+            return null;
+        } else {
+            System.err.println("Uknown state");
+        }
+        return null;
     }
 
     public boolean hasWalls(){
+        return this.wall;
 
-        return false;
     }
 
     public boolean hasWall(int dir){
 
-        return false;
+
     }
 
-    private boolean isEmpty() {
-        if(Square){
 
-        }
-    }
 }
--- src/test/java/inf112/skeleton/app/testSquare.java
+++ src/test/java/inf112/skeleton/app/testSquare.java
@@ -2,20 +2,40 @@ package inf112.skeleton.app;
 
 import org.junit.Test;
 
+import org.junit.Assert;
+
+import inf112.skeleton.app.Square;
+
+
+
 public class testSquare {
 
+
+
     @Test
-    public void gotRobot () {
+    public void squareIsEmpty () {
+        Square sqaure = new Square(5,5,"empty",true);
+        Assert.assertEquals(sqaure.getRobot(),null);
 
     }
 
     @Test
-    public static void hasWalls(){
+    public void squareContainsRobot () {
+        Square sqaure = new Square(5,5,"robot",false);
+        //Robot robot = new Robot();
+        //Assert.assertEquals(sqaure.getRobot(),robot);
 
     }
 
     @Test
-    public static void hasWallsWithDirection() {
+    public void hasWalls(){
+        Square square = new Square(5,5,"empty",true);
+        //Assert.assertTrue(true);
+
+    }
+
+    @Test
+    public void hasWallsWithDirection() {
         
     }
 }

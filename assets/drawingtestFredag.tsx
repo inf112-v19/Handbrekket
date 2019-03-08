baeebda9dd05c7c48ce56d3b7aec3325416a315d
bernhus <46955333+bernhus@users.noreply.github.com>
2019-03-08 13:16:04 +0100

    add properties to tileset

--- assets/drawingtestFredag.tsx
+++ assets/drawingtestFredag.tsx
@@ -1,4 +1,158 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.2" name="drawingtest" tilewidth="80" tileheight="80" tilecount="36" columns="6">
  <image source="drawingtest.png" width="481" height="480"/>
+ <tile id="0">
+  <properties>
+   <property name="retning" value="up"/>
+   <property name="type" value="straigthArrow"/>
+   <property name="value" value="2"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="retning" value="down"/>
+   <property name="type" value="straigthArrow"/>
+   <property name="value" value="3"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" value="hole"/>
+   <property name="value" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="retning" value="right"/>
+   <property name="type" value="straigthArrow"/>
+   <property name="value" value="1"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="retning" value="left"/>
+   <property name="type" value="straigthArrow"/>
+   <property name="value" value="0"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="vanlig"/>
+   <property name="type" value="wrench"/>
+   <property name="value" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" value="0"/>
+   <property name="type" value="wrench"/>
+   <property name="value" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="retning" value="right"/>
+   <property name="rotate" type="bool" value="true"/>
+   <property name="type" value="rigthArrow"/>
+   <property name="value" value="1"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="retning" value="down"/>
+   <property name="rotate" type="bool" value="true"/>
+   <property name="type" value="rigthArrow"/>
+   <property name="value" value="3"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="hammer"/>
+   <property name="type" value="wrench"/>
+   <property name="value" type="int" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="0"/>
+   <property name="type" value="wrench"/>
+   <property name="value" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="retning" value="up"/>
+   <property name="rotate" type="bool" value="true"/>
+   <property name="type" value="rigthArrow"/>
+   <property name="value" value="2"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="retning" value="left"/>
+   <property name="rotate" type="bool" value="true"/>
+   <property name="type" value="rigthArrow"/>
+   <property name="value" value="0"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="name" value="flag1"/>
+   <property name="type" value="wrench"/>
+   <property name="value" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="name" value="flag2"/>
+   <property name="type" value="wrench"/>
+   <property name="value" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="retning" value="down"/>
+   <property name="rotate" type="bool" value="false"/>
+   <property name="type" value="leftArrow"/>
+   <property name="value" value="3"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="retning" value="left"/>
+   <property name="rotate" type="bool" value="false"/>
+   <property name="type" value="leftArrow"/>
+   <property name="value" value="0"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="name" value="flag3"/>
+   <property name="type" value="wrench"/>
+   <property name="value" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="name" value="flag4"/>
+   <property name="type" value="wrench"/>
+   <property name="value" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="retning" value="right"/>
+   <property name="rotate" type="bool" value="false"/>
+   <property name="type" value="leftArrow"/>
+   <property name="value" value="1"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="retning" value="up"/>
+   <property name="rotate" type="bool" value="false"/>
+   <property name="type" value="leftArrow"/>
+   <property name="value" value="2"/>
+  </properties>
+ </tile>
 </tileset>

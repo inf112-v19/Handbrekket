a71e64566b6f80117a10b3b4f0dd9b97dd9db9a2
= <mari.mundal@gmail.com>
2019-03-08 15:49:09 +0100

    updated ProgramRegistersTests

--- src/test/java/inf112/skeleton/app/ProgramRegistersTest.java
+++ src/test/java/inf112/skeleton/app/ProgramRegistersTest.java
@@ -1,4 +1,6 @@
 package inf112.skeleton.app;
+import inf112.skeleton.app.card.ICardMovement;
+import inf112.skeleton.app.card.MovementCard;
 import inf112.skeleton.app.robot.IRobot;
 import inf112.skeleton.app.robot.Robot;
 import org.junit.Test;
@@ -16,6 +18,38 @@ public class ProgramRegistersTest {
         IRobot robot = new Robot(1);
         IProgramRegisters pR = new ProgramRegisters(robot);
         assertEquals(false, pR.isCardSlotsFilled());
+
+        assertFalse(true == pR.isCardSlotsFilled());
+    }
+
+    @Test
+    public void powerDownTest(){
+        IRobot robot = new Robot(1);
+        IProgramRegisters pR = new ProgramRegisters(robot);
+        assertEquals(false, pR.isPoweredDown());
+        pR.powerDown();
+        assertEquals(true, pR.isPoweredDown());
+        assertFalse(false == pR.isPoweredDown());
+    }
+
+    @Test
+    public void LivesTests(){
+        IRobot robot = new Robot(1);
+        IProgramRegisters pR = new ProgramRegisters(robot);
+        assertEquals(3, pR.howManyLivesLeft());
+        pR.removeLife();
+        pR.removeLife();
+        assertEquals(1, pR.howManyLivesLeft());
+    }
+
+    @Test
+    public void amountOfDamageTest(){
+        IRobot robot = new Robot(1);
+        IProgramRegisters pR = new ProgramRegisters(robot);
+        assertEquals(0, pR.amountOfDamage());
+        robot.changeHP(4);
+        assertEquals(4, pR.amountOfDamage());
+        assertFalse(9 == pR.amountOfDamage());
     }
 
 }

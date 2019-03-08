41527b823200208c58e29c8fdf81e77f8e15acbb
bernhus <46955333+bernhus@users.noreply.github.com>
2019-03-08 12:44:35 +0100

    changed tilemap

---
+++ assets/drawingtestFredag.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="drawingtest" tilewidth="80" tileheight="80" tilecount="36" columns="6">
+ <image source="drawingtest.png" width="481" height="480"/>
+</tileset>

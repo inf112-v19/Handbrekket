bb11e48745bcdbf0f6228b85356c718ab36dfe5d
nst016 <nichola.stephen@student.uib.no>
2019-03-08 15:27:29 +0100

    Metodes to constract card metodes

--- src/main/java/inf112/skeleton/app/game/Game.java
+++ src/main/java/inf112/skeleton/app/game/Game.java
@@ -146,5 +146,54 @@ public class Game implements IGame {
      */
     public void removeCard(boolean[] cards) {
 
+    }/**
+     * Creates a deck of programCards of all of the "simple" types, stored in the ArrayList programCards
+     * As of right now it's hard-coded, but in the future we should probably make it more dynamic
+     */
+    private void createDeck() {
+        //Creates the cards "Backup", "Move 1", "Move 2", "Move 3"
+        createMovementCards(6,10,430,-1);
+        createMovementCards(18,10,490,1);
+        createMovementCards(12,10,670,2);
+        createMovementCards(6, 10,790,3);
+
+        //Creates the cards "U-Turn", "Rotate Right", "Rotate Left"
+        createRotationCards(6,  10,10,true, 2);
+        createRotationCards(18, 20,80,true, 1);
+        createRotationCards(18, 20,70,false,1);
+    }
+
+    /**
+     * Creates a set of movement cards based on the input given
+     * @param cardNum The number of cards to create
+     * @param interval The interval between each card's priority
+     * @param intervalStart The starting value of the priority
+     * @param moveValue The amount of movement of the card
+     */
+    private void createMovementCards(int cardNum, int interval, int intervalStart, int moveValue) {
+        for(int i = 0; i < cardNum; i++){
+            programCards.add(new MovementCard(intervalStart + i * interval,moveValue));
+        }
+    }
+
+    /**
+     * Creates a set of rotation cards based on the input given
+     * @param cardNum The number of cards to create
+     * @param interval The interval between each card's priority
+     * @param intervalStart The starting value of the priority
+     * @param rotationDirection The direction of the rotation (TRUE = right, FALE = left)
+     * @param rotationValue The amount of rotation to be performed
+     */
+    private void createRotationCards(int cardNum, int interval, int intervalStart, boolean rotationDirection, int rotationValue) {
+        for(int i = 0; i < cardNum; i++){
+            programCards.add(new RotationCard(intervalStart + i * interval, rotationDirection, rotationValue));
+        }
+    }
+
+    /**
+     * Shuffles the deck of programCards
+     */
+    private void shuffleDeck() {
+        Collections.shuffle(programCards);
     }
 }

9c738e40c06e27ac9a3871b0fb75ff42e449f8f1
bernhus <marius.bernhus@gmail.com>
2019-03-15 14:19:03 +0100

    changed tiles

--- assets/drawingtestFredag.tsx
+++ assets/drawingtestFredag.tsx
@@ -3,15 +3,15 @@
  <image source="drawingtest.png" width="481" height="480"/>
  <tile id="0">
   <properties>
-   <property name="retning" value="up"/>
-   <property name="type" value="straigthArrow"/>
+   <property name="direction" value="up"/>
+   <property name="type" value="straightArrow"/>
    <property name="value" value="2"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
-   <property name="retning" value="down"/>
-   <property name="type" value="straigthArrow"/>
+   <property name="direction" value="down"/>
+   <property name="type" value="straightArrow"/>
    <property name="value" value="3"/>
   </properties>
  </tile>
@@ -23,45 +23,38 @@
  </tile>
  <tile id="6">
   <properties>
-   <property name="retning" value="right"/>
-   <property name="type" value="straigthArrow"/>
+   <property name="direction" value="right"/>
+   <property name="type" value="straightArrow"/>
    <property name="value" value="1"/>
   </properties>
  </tile>
  <tile id="7">
   <properties>
-   <property name="retning" value="left"/>
-   <property name="type" value="straigthArrow"/>
+   <property name="direction" value="left"/>
+   <property name="type" value="straightArrow"/>
    <property name="value" value="0"/>
   </properties>
  </tile>
  <tile id="8">
   <properties>
-   <property name="name" value="vanlig"/>
-   <property name="type" value="wrench"/>
-   <property name="value" type="int" value="0"/>
-  </properties>
- </tile>
- <tile id="9">
-  <properties>
-   <property name="name" value="0"/>
+   <property name="name" value="normal"/>
    <property name="type" value="wrench"/>
    <property name="value" type="int" value="0"/>
   </properties>
  </tile>
  <tile id="12">
   <properties>
-   <property name="retning" value="right"/>
+   <property name="direction" value="right"/>
    <property name="rotate" type="bool" value="true"/>
-   <property name="type" value="rigthArrow"/>
+   <property name="type" value="rightArrow"/>
    <property name="value" value="1"/>
   </properties>
  </tile>
  <tile id="13">
   <properties>
-   <property name="retning" value="down"/>
+   <property name="direction" value="down"/>
    <property name="rotate" type="bool" value="true"/>
-   <property name="type" value="rigthArrow"/>
+   <property name="type" value="rightArrow"/>
    <property name="value" value="3"/>
   </properties>
  </tile>
@@ -72,26 +65,19 @@
    <property name="value" type="int" value="-1"/>
   </properties>
  </tile>
- <tile id="15">
-  <properties>
-   <property name="name" value="0"/>
-   <property name="type" value="wrench"/>
-   <property name="value" type="int" value="0"/>
-  </properties>
- </tile>
  <tile id="18">
   <properties>
-   <property name="retning" value="up"/>
+   <property name="direction" value="up"/>
    <property name="rotate" type="bool" value="true"/>
-   <property name="type" value="rigthArrow"/>
+   <property name="type" value="rightArrow"/>
    <property name="value" value="2"/>
   </properties>
  </tile>
  <tile id="19">
   <properties>
-   <property name="retning" value="left"/>
+   <property name="direction" value="left"/>
    <property name="rotate" type="bool" value="true"/>
-   <property name="type" value="rigthArrow"/>
+   <property name="type" value="rightArrow"/>
    <property name="value" value="0"/>
   </properties>
  </tile>
@@ -111,7 +97,7 @@
  </tile>
  <tile id="24">
   <properties>
-   <property name="retning" value="down"/>
+   <property name="direction" value="down"/>
    <property name="rotate" type="bool" value="false"/>
    <property name="type" value="leftArrow"/>
    <property name="value" value="3"/>
@@ -119,7 +105,7 @@
  </tile>
  <tile id="25">
   <properties>
-   <property name="retning" value="left"/>
+   <property name="direction" value="left"/>
    <property name="rotate" type="bool" value="false"/>
    <property name="type" value="leftArrow"/>
    <property name="value" value="0"/>
@@ -141,7 +127,7 @@
  </tile>
  <tile id="30">
   <properties>
-   <property name="retning" value="right"/>
+   <property name="direction" value="right"/>
    <property name="rotate" type="bool" value="false"/>
    <property name="type" value="leftArrow"/>
    <property name="value" value="1"/>
@@ -149,7 +135,7 @@
  </tile>
  <tile id="31">
   <properties>
-   <property name="retning" value="up"/>
+   <property name="direction" value="up"/>
    <property name="rotate" type="bool" value="false"/>
    <property name="type" value="leftArrow"/>
    <property name="value" value="2"/>

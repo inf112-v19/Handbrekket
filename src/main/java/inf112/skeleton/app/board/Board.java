aac541a9268f7ab903f20f1290e61d456f9a20c8
ettosan <aledaable@gmail.com>
2019-03-20 00:23:59 +0100

    BoardTest
    Tested for some of the methods in board. Tested for width, height and getmap.
    Need to test for checkSquare next.
    
    Projectboard : https://github.com/inf112-v19/Handbrekket/projects/2

--- src/main/java/inf112/skeleton/app/board/Board.java
+++ src/main/java/inf112/skeleton/app/board/Board.java
@@ -60,9 +60,63 @@ public class Board implements IBoard {
 		return board.get(x).get(y);
 	}
 
+
+
 	@Override
+<<<<<<< Updated upstream
 	public void setSquare(int x, int y) {
 		board.get(x).set(y, new Square(0,0));
+=======
+	public BoardElements checkSquare(int xCoordinate, int yCoordinate) {
+		TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(0);
+		TiledMapTileLayer.Cell cell = layer.getCell(xCoordinate,yCoordinate);
+		String type = cell.getTile().getProperties().get("type").toString();
+
+		BoardElements elementType = null;
+		switch (type) {
+			case "straightArrow":
+				String dir = cell.getTile().getProperties().get("direction").toString();
+				switch (dir) {
+					case "north": elementType = BoardElements.CONVEYORBELT_MOVE_NORTH; break;
+					case "south": elementType = BoardElements.CONVEYORBELT_MOVE_SOUTH; break;
+					case "west": elementType = BoardElements.CONVEYORBELT_MOVE_WEST; break;
+					case "east": elementType = BoardElements.CONVEYORBELT_MOVE_EAST; break;
+				}
+				break;
+			case "rightArrow":
+				dir = cell.getTile().getProperties().get("direction").toString();
+				switch (dir) {
+					case "north": elementType = BoardElements.CONVEYORBELT_TURN_RIGHT_MOVE_NORTH; break;
+					case "south": elementType = BoardElements.CONVEYORBELT_TURN_RIGHT_MOVE_SOUTH; break;
+					case "west": elementType = BoardElements.CONVEYORBELT_TURN_RIGHT_MOVE_WEST; break;
+					case "east": elementType = BoardElements.CONVEYORBELT_TURN_RIGHT_MOVE_EAST; break;
+				}
+				break;
+            case "leftArrow":
+			    dir = cell.getTile().getProperties().get("direction").toString();
+			    switch (dir) {
+				    case "north": elementType = BoardElements.CONVEYORBELT_TURN_LEFT_MOVE_NORTH; break;
+				    case "south": elementType = BoardElements.CONVEYORBELT_TURN_LEFT_MOVE_SOUTH; break;
+				    case "west": elementType = BoardElements.CONVEYORBELT_TURN_LEFT_MOVE_WEST; break;
+				    case "east": elementType = BoardElements.CONVEYORBELT_TURN_LEFT_MOVE_EAST; break;
+			    }
+			    break;
+			case "hole": elementType = BoardElements.HOLES; break;
+			case "wrench":
+			    //TODO: change from int value to String name
+                int value = (int) cell.getTile().getProperties().get("value");
+                switch (value) {
+                    case 0: elementType = BoardElements.WRENCH; break;
+                    case -1: elementType = BoardElements.SPECIAL_WRENCH; break;
+                    case 1: elementType = BoardElements.FLAG1; break;
+                    case 2: elementType = BoardElements.FLAG2; break;
+                    case 3: elementType = BoardElements.FLAG3; break;
+                    case 4: elementType = BoardElements.FLAG4; break;
+                }
+
+		}
+		return elementType;
+>>>>>>> Stashed changes
 	}
 
 }

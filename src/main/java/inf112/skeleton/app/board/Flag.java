3935c1b31948d3ccbfafc5371d25a719a228f2b9
Eirik Ravnskog <era009@student.uib.no>
2019-03-24 13:02:48 +0100

    Implemented a new class; Flag

---
+++ src/main/java/inf112/skeleton/app/board/Flag.java
@@ -0,0 +1,57 @@
+package inf112.skeleton.app.board;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import inf112.skeleton.app.robot.IRobot;
+
+
+
+public class Flag implements IFlag {
+
+    public int flagNumber;
+    public int y;
+    public int x;
+    Sprite sprite;
+
+    /**
+     * @param flag
+     * @param map
+     */
+    public Flag(@org.jetbrains.annotations.NotNull RectangleMapObject flag, TiledMap map) {
+        x = (int) flag.getRectangle().getX();
+        y = (int) flag.getRectangle().getY();
+
+        Texture texture = new Texture(".png");
+        sprite = new Sprite(texture);
+        sprite.setSize(5, 5);
+    }
+
+    @Override
+    public int getFlagNumber() {
+        return flagNumber;
+    }
+
+    @Override
+    public Sprite getSprite() {
+        sprite.setX(x);
+        sprite.setY(y);
+        return sprite;
+    }
+
+    @Override
+    public void updateArchiveLocation(IRobot robot) {
+        int[] backup = new int[2];
+        backup[0] = x;
+        backup[1] = y;
+        robot.setBackup(backup);
+    }
+}
+
+
+
+
+
+
+

05b70b953b7c780b6191fdcfe58fef7c8cfe8210
JonasLindaas <46955316+JonasLindaas@users.noreply.github.com>
2019-03-25 13:17:27 +0100

    Corrected misspelling of GameRuleConstant

--- src/main/java/inf112/skeleton/app/game/GameRuleConstants.java
+++ src/main/java/inf112/skeleton/app/game/GameRuleConstants.java
@@ -3,7 +3,7 @@ package inf112.skeleton.app.game;
 public enum GameRuleConstants {
     MAX_LIVES(3), MAX_DAMAGE(9), MAX_CARDS_IN_REGISTER(9), ACTIVE_CARDS_IN_REGISTER(5);
 
-    GameRuleConstans (int value){
+    GameRuleConstants (int value){
         this.value=value;
     }
 

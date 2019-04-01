96a9b0678b8a9696aa6ce60197182fb26df06cba
bernhus <marius.bernhus@gmail.com>
2019-04-01 10:58:48 +0200

    add properties to tiledmap

--- assets/drawingtestFredag.tsx
+++ assets/drawingtestFredag.tsx
@@ -1,9 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="drawingtest" tilewidth="80" tileheight="80" tilecount="36" columns="6">
- <image source="drawingtestWL.png" width="721" height="480"/>
+<tileset version="1.2" tiledversion="1.2.2" name="drawingtest" tilewidth="80" tileheight="80" tilecount="54" columns="9">
+ <image source="drawingtestWL.png" width="720" height="481"/>
  <tile id="0">
   <properties>
    <property name="direction" value="up"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="type" value="straightArrow"/>
    <property name="value" value="2"/>
   </properties>
@@ -11,6 +12,7 @@
  <tile id="1">
   <properties>
    <property name="direction" value="down"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="type" value="straightArrow"/>
    <property name="value" value="3"/>
   </properties>
@@ -21,9 +23,38 @@
    <property name="value" type="int" value="0"/>
   </properties>
  </tile>
+ <tile id="4">
+  <properties>
+   <property name="direction" value="up"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="type" value="straightArrow"/>
+   <property name="value" value="2"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="direction" value="down"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="type" value="straightArrow"/>
+   <property name="value" value="3"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="direction" value="NORTH"/>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="position" value="vertical"/>
+   <property name="type" value="laser"/>
+  </properties>
+ </tile>
  <tile id="9">
   <properties>
    <property name="direction" value="right"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="type" value="straightArrow"/>
    <property name="value" value="1"/>
   </properties>
@@ -31,6 +62,7 @@
  <tile id="10">
   <properties>
    <property name="direction" value="left"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="type" value="straightArrow"/>
    <property name="value" value="0"/>
   </properties>
@@ -42,9 +74,38 @@
    <property name="value" type="int" value="0"/>
   </properties>
  </tile>
+ <tile id="13">
+  <properties>
+   <property name="direction" value="right"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="type" value="straightArrow"/>
+   <property name="value" value="1"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="direction" value="left"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="type" value="straightArrow"/>
+   <property name="value" value="0"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="direction" value="SOUTH"/>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="position" value="horizontal"/>
+   <property name="type" value="laser"/>
+  </properties>
+ </tile>
  <tile id="18">
   <properties>
    <property name="direction" value="right"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="rotate" type="bool" value="true"/>
    <property name="type" value="rightArrow"/>
    <property name="value" value="1"/>
@@ -53,6 +114,7 @@
  <tile id="19">
   <properties>
    <property name="direction" value="down"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="rotate" type="bool" value="true"/>
    <property name="type" value="rightArrow"/>
    <property name="value" value="3"/>
@@ -65,9 +127,34 @@
    <property name="value" type="int" value="-1"/>
   </properties>
  </tile>
+ <tile id="22">
+  <properties>
+   <property name="direction" value="right"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="rotate" type="bool" value="true"/>
+   <property name="type" value="rightArrow"/>
+   <property name="value" value="1"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="direction" value="down"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="rotate" type="bool" value="true"/>
+   <property name="type" value="rightArrow"/>
+   <property name="value" value="3"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="direction" value=""/>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
  <tile id="27">
   <properties>
    <property name="direction" value="up"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="rotate" type="bool" value="true"/>
    <property name="type" value="rightArrow"/>
    <property name="value" value="2"/>
@@ -76,6 +163,7 @@
  <tile id="28">
   <properties>
    <property name="direction" value="left"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="rotate" type="bool" value="true"/>
    <property name="type" value="rightArrow"/>
    <property name="value" value="0"/>
@@ -95,9 +183,34 @@
    <property name="value" type="int" value="2"/>
   </properties>
  </tile>
+ <tile id="31">
+  <properties>
+   <property name="direction" value="up"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="rotate" type="bool" value="true"/>
+   <property name="type" value="rightArrow"/>
+   <property name="value" value="2"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="direction" value="left"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="rotate" type="bool" value="true"/>
+   <property name="type" value="rightArrow"/>
+   <property name="value" value="0"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="direction" value="EAST"/>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
  <tile id="36">
   <properties>
    <property name="direction" value="down"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="rotate" type="bool" value="false"/>
    <property name="type" value="leftArrow"/>
    <property name="value" value="3"/>
@@ -106,6 +219,7 @@
  <tile id="37">
   <properties>
    <property name="direction" value="left"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="rotate" type="bool" value="false"/>
    <property name="type" value="leftArrow"/>
    <property name="value" value="0"/>
@@ -125,9 +239,34 @@
    <property name="value" type="int" value="4"/>
   </properties>
  </tile>
+ <tile id="40">
+  <properties>
+   <property name="direction" value="down"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="rotate" type="bool" value="false"/>
+   <property name="type" value="leftArrow"/>
+   <property name="value" value="3"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="direction" value="left"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="rotate" type="bool" value="false"/>
+   <property name="type" value="leftArrow"/>
+   <property name="value" value="0"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="direction" value="WEST"/>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
  <tile id="45">
   <properties>
    <property name="direction" value="right"/>
+   <property name="isExpress" type="bool" value="false"/>
    <property name="rotate" type="bool" value="false"/>
    <property name="type" value="leftArrow"/>
    <property name="value" value="1"/>
@@ -136,6 +275,25 @@
  <tile id="46">
   <properties>
    <property name="direction" value="up"/>
+   <property name="isExpress" type="bool" value="false"/>
+   <property name="rotate" type="bool" value="false"/>
+   <property name="type" value="leftArrow"/>
+   <property name="value" value="2"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="direction" value="right"/>
+   <property name="isExpress" type="bool" value="true"/>
+   <property name="rotate" type="bool" value="false"/>
+   <property name="type" value="leftArrow"/>
+   <property name="value" value="1"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="direction" value="up"/>
+   <property name="isExpress" type="bool" value="true"/>
    <property name="rotate" type="bool" value="false"/>
    <property name="type" value="leftArrow"/>
    <property name="value" value="2"/>

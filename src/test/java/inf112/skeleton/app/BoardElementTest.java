e4bf0b6f4a36ddd5f994593739ddc5c62753c901
ettosan <aledaable@gmail.com>
2019-05-03 13:34:41 +0200

    made Boardelement test

---
+++ src/test/java/inf112/skeleton/app/BoardElementTest.java
@@ -0,0 +1,30 @@
+package inf112.skeleton.app;
+
+import inf112.skeleton.app.board.BoardElement;
+import inf112.skeleton.app.board.Direction;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BoardElementTest {
+
+    @Test
+    public void getDirectionTest () {
+        Direction directionFromMethod = BoardElement.WALL_EAST.getDirection();
+
+
+        Direction direction = Direction.EAST;
+
+        assertEquals(direction,directionFromMethod);
+    }
+
+    @Test
+    public void getValueTest() {
+        int valueFromMethod = BoardElement.WALL_EAST.getValue();
+        int value = -1;
+
+        assertEquals(value,valueFromMethod);
+
+    }
+
+}

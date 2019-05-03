41df07b4086d63c96a03e9853137050f7c210ba8
Jonas Drageset Lindaas <Jonas.Lindaas@student.uib.no>
2019-05-03 15:33:49 +0200

    Removed testClasses that did not work.

--- src/main/java/inf112/skeleton/app/game/IGame.java
+++ src/main/java/inf112/skeleton/app/game/IGame.java
@@ -16,170 +16,6 @@ import java.util.ArrayList;
  * game will implement board as field.
  */
 public interface IGame {
-
-<<<<<<< HEAD
-    /**
-     * Retrieves the board from IBoard
-     *
-     * @return board from IBoard
-     */
-    IBoard getBoard();
-
-
-    /**
-     * Absolute movement of the robot in the current register
-     *
-     * @param coordinate the new coordinate
-     */
-    void absoluteMove(IRobot robot, int[] coordinate);
-
-
-    /**
-     * Relative movement (robot)
-     *
-     * @param robot to be moved
-     * @param card  the movement card
-     */
-    void relativeMove(IRobot robot, ICardMovement card);
-
-
-    /**
-     * Checks if tile contains a robot and returns the robot
-     *
-     * @param coordinate x-ccordinate on index 0,
-     *                   y-ccordinate on index 1 on board
-     * @return IRobot robot
-     */
-    IProgramRegister checkIfContainsRobot(int[] coordinate);
-
-    /**
-     * Turns the current Robot
-     *
-     * @param robot the robot to be rotated
-     * @param card  the rotation card
-     */
-    void rotationMove(IRobot robot, ICardRotation card);
-
-    /**
-     * execute a phase
-     */
-    void progressPhase();
-
-
-    /**
-     * execute a round
-     */
-    void progressRound(GameGFX GraphicsInterface);
-
-
-    /**
-     * @return an ordered list of events
-     */
-    ArrayList<Event> makeEventList();
-
-
-    /**
-     * reads and executes events in order
-     */
-    Event readEvents(ArrayList<Event> listOfEvents);
-
-
-    /**
-     * repair the robot
-     *
-     * @param programRegister to be repaired
-     */
-    void repair(IProgramRegister programRegister);
-
-    /**
-     * updates the backup of the robot
-     *
-     * @param robot to update the backup of
-     */
-    void updateBackUp(IRobot robot);
-
-
-    /**
-     * Deals cards to all of the programRegisters
-     */
-    void dealCards();
-
-
-    /**
-     * first checks if there is a flag and a robot,
-     * then checks if the robot hits flags in right order.
-     * If so, updates the robot programming card.
-     * Finally, it always places a new backup.
-     */
-    void activateFlag();
-
-    /**
-     * removes the card
-     *
-     * @param cards
-     */
-    void removeCard(boolean[] cards);
-
-    /**
-     * Activate Coveyorbelts, can chose whether to activate all belts or only express
-     *
-     * @param activateOnlyExpressConveyorBelts true if you only want to activate express conveyors
-     */
-    void activateConveyorBelts(boolean activateOnlyExpressConveyorBelts);
-
-    /**
-     * Adds a card to the deck
-     *
-     * @param card to be added to the deck
-     */
-    void addCardToDeck(ICard card);
-
-    /**
-     * Checks if it's possible to go from one space to the other
-     * Has to be adjacent, if not, an exception will be thrown
-     *
-     * @param startCoordinates       the start of the movement
-     * @param destinationCoordinates the end of the movement
-     * @return true if possible, false if not
-     */
-    boolean canMove(int[] startCoordinates, int[] destinationCoordinates);
-
-    /**
-     * Checks if there is a wall in the given direction for the given position.
-     *
-     * @param position Position to check
-     * @param dir      Direction to check
-     * @return true if there is a wall, false if there is not.
-     */
-    boolean checkForWall(int[] position, Direction dir);
-
-    /**
-     * Checks if robot is on a flag-tile
-     *
-     * @param robot The robot to check
-     * @return true if it is on a flag, false otherwise
-     */
-    boolean checkIfOnFlag(IRobot robot);
-
-    /**
-     * Activate lasers
-     * Iterate over the robots and lasers
-     * Changes HP
-     */
-    void activateLasers();
-
-    /**
-     * Activate robot lasers
-     */
-    void activateRobotLasers();
-
-    /**
-     * Iterates through the registers and performs any repairs on robots on repairSites
-     *
-     * @return true if it is on a repairSite, false otherwise
-     */
-    void doRepairs();
-=======
 	/**
 	 * Retrieves the board from IBoard
 	 *
@@ -335,45 +171,15 @@ public interface IGame {
      * @return true if robot has won, false otherwise
      */
     IRobot winCheck();
->>>>>>> 8fcd8a6cf94e96a666da2b77566fade505d7d1f0
 
     /**
      * Ends the game if robot has won
      */
 
     boolean gameOver();
-<<<<<<< HEAD
-    /**
-     * Checks if a robot has won the game
-     *
-     * @return true if robot has won, false otherwise
-     */
-    boolean winCheck();
-
-    /**
-     * Checks if the game has any human players in it
-     *
-     * @return true if human players > 0, false if it's == 0
-     */
-    boolean checkIfGameHasHumanPlayers();
-=======
-
-	/**
-	 * Checks if a robot has won the game
-	 *
-	 * @return true if robot has won, false otherwise
-	 */
-	boolean winCheck();
 
 
-	/**
-	 * Checks if the game has any human players in it
-	 *
-	 * @return true if human players > 0, false if it's == 0
-	 */
-	boolean checkIfGameHasHumanPlayers();
-
->>>>>>> 8fcd8a6cf94e96a666da2b77566fade505d7d1f0
+	public boolean checkIfGameHasHumanPlayers();
 }
 
 

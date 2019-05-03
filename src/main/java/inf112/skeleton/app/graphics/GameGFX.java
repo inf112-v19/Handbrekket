29132ddddf29bd5fcb61233668e6f0d2eadae766
bernhus <marius.bernhus@gmail.com>
2019-05-03 15:21:53 +0200

    changed color

--- src/main/java/inf112/skeleton/app/graphics/GameGFX.java
+++ src/main/java/inf112/skeleton/app/graphics/GameGFX.java
@@ -249,7 +249,7 @@ public class GameGFX extends Stage {
     }
 
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 

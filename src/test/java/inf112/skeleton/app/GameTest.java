e1e989509b2e076a52b8043da0d30302eee00e11
ettosan <aledaable@gmail.com>
2019-05-03 14:11:25 +0200

    added to Lasertest so it covers all methods

--- src/test/java/inf112/skeleton/app/GameTest.java
+++ src/test/java/inf112/skeleton/app/GameTest.java
@@ -24,30 +24,30 @@ public class GameTest {
     private ICardRotation moveRight;
     private ICardRotation moveLeft;
     private ICardRotation uTurn;
-    private GameState gameState;
-    private ArrayList<ICard> deck = new ArrayList<>();
+    //private GameState gameState;
+    // private ArrayList<ICard> deck = new ArrayList<>();
 
 
     @Before
     public void setUp() {
-        int[] testCoordinates = {1,1};
-        robot = new Robot(0,testCoordinates);
+        int[] testCoordinates = {1, 1};
+        robot = new Robot(0, testCoordinates);
         robot.setDir(Direction.EAST);
         TiledMap map = new TiledMap();
-        game = new Game(map,1,2);
+        game = new Game(map, 1, 2);
     }
 
     @Test
     public void testMoveTwoSteps() {
         game.relativeMoveStraight(robot, Direction.EAST, 2);
-        int [] newPos = {3,1};
+        int[] newPos = {3, 1};
         assertEquals(robot.getPosition(), newPos);
     }
 
     @Test
     public void testMoveOneStep() {
         game.relativeMoveStraight(robot, Direction.EAST, 1);
-        int [] newPos = {2,1};
+        int[] newPos = {2, 1};
         assertEquals(robot.getPosition(), newPos);
     }
 
@@ -55,95 +55,89 @@ public class GameTest {
     @Test
     public void testMoveThreeStep() {
         game.relativeMoveStraight(robot, Direction.EAST, 3);
-        int [] newPos = {4,1};
+        int[] newPos = {4, 1};
         assertEquals(robot.getPosition(), newPos);
     }
 
     @Test
     public void testMoveOneBackwards() {
         game.relativeMoveStraight(robot, Direction.EAST, -1);
-        int [] newPos = {0,1};
+        int[] newPos = {0, 1};
         assertEquals(robot.getPosition(), newPos);
     }
 
     @Test
     public void testRotateRight() {
-        RotationCard moveRight = new RotationCard(1,true,1);
+        RotationCard moveRight = new RotationCard(1, true, 1);
         game.rotationMove(robot, moveRight);
         assertEquals(robot.getDir(), Direction.SOUTH);
     }
 
     @Test
     public void testRotateLeft() {
-        RotationCard moveLeft = new RotationCard(1,false,1);
+        RotationCard moveLeft = new RotationCard(1, false, 1);
         game.rotationMove(robot, moveLeft);
         assertEquals(robot.getDir(), Direction.NORTH);
     }
 
     @Test
     public void testUTurn() {
-        RotationCard uTurn = new RotationCard(1,true,2);
+        RotationCard uTurn = new RotationCard(1, true, 2);
         game.rotationMove(robot, uTurn);
         assertEquals(robot.getDir(), Direction.WEST);
     }
 
-<<<<<<< HEAD
+
 /**
-=======
+ =======
+ /*
+ >>>>>>> 77be1b0fe4a7ad6688f9ee7f177b369244bd6336
+ @Test public void checkIfOnHoleTest() {
+ boardHoles.add(3);
+ game.relativeMoveStraight(robot, Direction.EAST, 2);
+ assertTrue(game.checkIfOnHoleOrOutSideBoard(robot));
+ }
+
+ @Test public void checkIfOnFlagTest() {
+ boardFlags.add(2);
+ game.relativeMoveStraight(robot, Direction.EAST, 1);
+ assertTrue(game.checkIfOnFlag(robot));
+ }
+
+ @Test public void checkIfOutsideBoardTest() {
+
+ //Tests that robot really is outside the given board
+ Board board = new Board(8, 7, mapIn);
+ int[] position = new position[2]; //robot's position
+ position[0] = 10;
+ position[1] = 10;
+ assertTrue(game.checkIfOutsideBoard(position));
+
+ //Test that robot is not outside the given board
+ position[0] = 4;
+ position[1] = 4;
+
+ assertFalse(game.checkIfOutsideBoard(position));
+
+ }
+
+ @Test public void checkForWallTest() {
+ boardWalls.add(3);
+ game.relativeMoveStraight(robot, Direction.EAST, 2);
+ assertTrue(game.checkForWall(robot.getPosition(), Direction.NORTH));
+ }
+
+ @Test public void noCrashWithWallTest() {
+ game.relativeMoveStraigt(robot, Direction.EAST, 3);
+ int[] robotPos = new int[2];
+ robotPos[0] = robot.getPosition(); //new x-coordinate
+ robotPos[1] = robot.getPosition(); //new y-coordinate
+ assertEquals(2, robotPos[0]); //only moved two steps (not three!)
+ assertEquals(1, robotPos[1]);
+ <<<<<<< HEAD
+ }*/
+
 /*
->>>>>>> 77be1b0fe4a7ad6688f9ee7f177b369244bd6336
-    @Test
-    public void checkIfOnHoleTest() {
-        boardHoles.add(3);
-        game.relativeMoveStraight(robot, Direction.EAST, 2);
-        assertTrue(game.checkIfOnHoleOrOutSideBoard(robot));
-    }
-
-    @Test
-    public void checkIfOnFlagTest() {
-        boardFlags.add(2);
-        game.relativeMoveStraight(robot, Direction.EAST, 1);
-        assertTrue(game.checkIfOnFlag(robot));
-    }
-
-    @Test
-    public void checkIfOutsideBoardTest() {
-
-        //Tests that robot really is outside the given board
-        Board board = new Board(8, 7, mapIn);
-        int[] position = new position[2]; //robot's position
-        position[0] = 10;
-        position[1] = 10;
-        assertTrue(game.checkIfOutsideBoard(position));
-
-        //Test that robot is not outside the given board
-        position[0] = 4;
-        position[1] = 4;
-
-        assertFalse(game.checkIfOutsideBoard(position));
-
-    }
-
-    @Test
-    public void checkForWallTest() {
-        boardWalls.add(3);
-        game.relativeMoveStraight(robot, Direction.EAST, 2);
-        assertTrue(game.checkForWall(robot.getPosition(), Direction.NORTH));
-    }
-
-    @Test
-    public void noCrashWithWallTest() {
-        game.relativeMoveStraigt(robot, Direction.EAST, 3);
-        int[] robotPos = new int[2];
-        robotPos[0] = robot.getPosition(); //new x-coordinate
-        robotPos[1] = robot.getPosition(); //new y-coordinate
-        assertEquals(2, robotPos[0]); //only moved two steps (not three!)
-        assertEquals(1, robotPos[1]);
-<<<<<<< HEAD
-    }*/
-=======
-    }
-
     @Test
     public void getGameStateTest() {
         gameState = GameState.DEALING_CARDS;
@@ -207,7 +201,7 @@ public class GameTest {
         ArrayList<IConveyorBelt> conveyorBelts = new ArrayList<>();
         conveyorBelts.add(3);
         assertArrayEquals(conveyorBelt.getPosition(), position);
-/*
+
         private IConveyorBelt getConveyorInPosition(int[] position) {
             for(IConveyorBelt conveyorBelt : conveyorBelts) {
                 if(Arrays.equals(conveyorBelt.getPosition(), position))
@@ -238,5 +232,5 @@ public class GameTest {
 
 
 **/
->>>>>>> 77be1b0fe4a7ad6688f9ee7f177b369244bd6336
+
 }
